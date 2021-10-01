e8fe8ac22408c71094f6e0103e7fb5f018d84203
decentlevi <decentlevi@gmail.com>
2021-10-01 01:49:45 +0100

    feature(About Page): implementation of about page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: 'Portfolio | Home', active: 'home' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Portfolio | About', active: 'about' });
+});
+
 module.exports = router;
