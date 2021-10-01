d3e7ae8cc81a49f70bd09757f918dc6340e98e75
decentlevi <decentlevi@gmail.com>
2021-10-01 01:24:21 +0100

    feature(Home Page): implementation of home page

---
+++ public/javascripts/index.js
@@ -0,0 +1,22 @@
+/**
+ * File Name: 
+ * 
+ * Student ID:
+ * 
+ * Student Name:
+ * 
+ * Date:
+ */ 
+
+(function () {
+
+    // Animation on scroll
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        });
+    });
+})()
